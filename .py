e720ca27ce7fc3c7944f887004344dcffee636dc
sebastianfacuse <facuseseba13@gmailcom>
2025-04-17 12:54:48 -0400

    2d clas

---
+++ .py
@@ -0,0 +1,11 @@
+# nombre=input("ingrese un nombre")
+# for i in range (3):
+#     print("hola", nombre)
+notas=0
+print("bienvenido")
+for i in range (3):
+    nota=float(input("hola ingrese su nota"))
+    notas=notas+nota
+promedio=notas/3
+print("su promedio es", promedio)
+
