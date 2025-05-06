7aeb387bcae40245181451a67f6150f90c4c4235
sebastianfacuse <facuseseba13@gmail.com>
2025-05-06 13:21:24 -0400

    miau

--- ejercicios/ejercicios_de_prueba.py
+++ ejercicios/ejercicios_de_prueba.py
@@ -45,10 +45,66 @@
 
  ##BATALLA POKEMON
 
-hp_pokemon1=50
-hp_pokemon2=50
-pokemon1=str(input("ingrese el nombre de su pokemon"))
-pokemon2=str(input("ingrese el nombre de su pokemon"))
-while hp_pokemon1 or hp_pokemon2!=0:
-    print(f"turno de {pokemon1}")
-    
+# hp_pokemon1=50
+# hp_pokemon2=50
+# pokemon1=str(input("ingrese el nombre de su pokemon"))
+# pokemon2=str(input("ingrese el nombre de su pokemon"))
+# while hp_pokemon1 or hp_pokemon2!=0:
+#     print(f"turno de {pokemon1}")
+""""""""
+# Calcular el puntaje de credito
+# De bede calcular que tanto credito tiene una persona
+# en cierta entidad financiera, debera considerar
+# cantidad de ingresos, nivel educacional y nacionalidad
+# Cantidad de ingresos
+# 500.000 a 1.000.000 : 300.000
+# 1.000.000 a 1.500.000: 650.000
+# 1.500.001 o mas : 1.000.000
+# Nivel Educacional 
+# Basico : x1, medio: x1.3 , superior: x1.5
+# Nacionalidad 
+# Chilena : +300.000, Extranjero: +0
+# cont=0
+# op=0
+# while op!=1 and op!=2 and op!=3:
+#      print(f"1-500.000$ a 1.000.000$")
+#      print(f"2-1.000.000$ a 1.500.000$")
+#      print(f"3-1.500.000$ o mas")
+#      op=int(input("ingrese su sueldo en base a estos rangos"))
+#      if op==1:
+#           cont+=300000
+#      elif op==2:
+#           cont+=650000
+#      elif op==3:
+#           cont+=1000000
+# ap=0
+# while ap!=1 and ap!=2 and ap!=3:
+#      print("1-basica")
+#      print("2-media")
+#      print("3-superior")
+#      ap=int(input("ingrese su opcion"))
+#      if ap==1:
+#           cont*=1
+#      elif ap==2:
+#           cont*=1.3
+#      elif ap==3:
+#           cont*=1.5
+# ep=0
+# while ep!=1 and ep!=2:
+#      print("1-chilena")
+#      print("2-otro")
+#      ep=int(input("ingrese su nacionalidad"))
+#      if ep==1:
+#           print(f"recibe un bono de 300.000$ y su credito es de {cont+300000}")
+#      else:
+#           print(f"su credito es de {cont}")
+#####
+num1=int(input("ingrese un numero"))
+num2=0
+while num1>=num2:   
+    num2=int(input("ingrese otro numero que sea mayor al primero"))
+import random
+num_random=random.randint(num1,num2)
+print (f"el numero aleatorio es {num_random}")
+for i in range (num_random):
+    print("onichan")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
