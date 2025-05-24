be03e10c74d39dec8436e71f40ace44a96ddc68c
sebastianfacuse <facuseseba13@gmail.com>
2025-05-23 23:15:28 -0400

    zorruaaaaaa

--- ejercicios_repaso_prueba3.py
+++ ejercicios_repaso_prueba3.py
@@ -158,90 +158,203 @@
 # print(f"El pago más alto registrado fue: ${max(pagos_realizados)}")
 ##########
 ##########
-import random,time
-espada_diamante=0
-diamante=0
-palo=0
-carbon=0
-resta1=2
-resta2=1
+# import random,time
+# espada_diamante=0
+# diamante=0
+# palo=0
+# carbon=0
+# resta1=2
+# resta2=1
+# while True:
+#     try:
+#         op=int(input("""que desea hacer?
+#                     1-minar
+#                     2-craftear
+#                     3-abrir el inventario
+#                     4-salir"""))
+#         match op:
+#             case 1:
+#                     minar=int(input("""que desea minar?
+#                                     1-diamantes
+#                                     2-palos"""))
+#                     while True:
+#                         match minar:
+#                             case 1:
+#                                 print("minando")
+#                                 # time.sleep(3)
+#                                 probabilidad=random.randint(1,7)
+#                                 if probabilidad==1:
+#                                     print("usted ha minado un diamante!!!")
+#                                     diamante+=1
+#                                 else:
+#                                     print("usted consiguio un carbon")
+#                                     carbon+=1
+#                                 salir=int(input("""desea salir?
+#                                                 1-si
+#                                                 2-no"""))
+#                                 match salir:
+#                                     case 1:
+#                                         print("saliendo")
+#                                         break
+#                             case 2:
+#                                 print("minando")
+#                                 # time.sleep(3)
+#                                 probabilidad=random.randint(1,3)
+#                                 if probabilidad==1:
+#                                     print("usted ha minado un palo")
+#                                     palo+=1
+#                                 else:
+#                                     print("consigui un carbon")
+#                                     carbon+=1
+#                                     salir=int(input("""desea salir?
+#                                                 1-si
+#                                                 2-no"""))
+#                                 match salir:
+#                                     case 1:
+#                                         print("saliendo")
+#                                         break
+#             case 2:
+#                 while True:
+#                     print("necesita dos diamantes y un palo para craftear una espada")
+#                     espada=int(input("""desea craftear una espada de diamante?
+#                                     1-si
+#                                     2-no"""))
+#                     match op:
+#                         case 1:
+#                             if diamante<=1 or palo<=0:
+#                                 print("usted no tiene recursos suficientes, consiga recursos para craftear una espada de diamante")
+#                             elif diamante>=2 and palo>=1:
+#                                 palo-=1
+#                                 diamante-=2
+#                                 print("usted ha crafteado una espada de diamante")
+#                                 espada_diamante+=1
+#                                 print(f"espada de diamante={espada_diamante}")
+#                         case 2:
+#                             print("saliendo")
+#                             break
+#             case 3:
+#                 print(f"espada de diamante={espada_diamante}")
+#                 print(f"diamantes={diamante}")
+#                 print(f"palos={palo}")
+#                 print(f"carbon={carbon}")
+#             case 4:
+#                 print("saliendo")
+#                 break
+#     except Exception:
+#         print("ingrese una opcion valida")
+############
+############
+# una farmacia donde sea por ticket aleatorio y muestre total de compras y total de tickets sacados y total ganados
+import random
+ganancias=[]
+total_compras=0
+total_usuario=0
+paracetamol=0
+dipirona=0
+algodon=0
+alcohol=0
+intento1=3
+intento2=3
+intento3=3
+nombre=str(input("ingrese su nombre "))
 while True:
     try:
-        op=int(input("""que desea hacer?
-                    1-minar
-                    2-craftear
-                    3-abrir el inventario
-                    4-salir"""))
+        pase=int(input("ingrese su contraseña "))
+        break
+    except Exception:
+        print("solo debe contener numeros su contraseña")
+while True:
+    try:
+        rut=int(input("ingrese su rut sin puntos ni guiones "))
+        ticket=random.randint(1,400)
+        print(f"su ticket es el numero:{ticket}")
+        break
+    except Exception:
+        print("porfavor ingrese solo numeros, sin puntuacion ni letras")
+while True:
+    try:
+        total_usuario=0
+        op=int(input("""ingrese la opcion que desea
+                    1-menu de compras
+                    2-salir """))
         match op:
             case 1:
-                    minar=int(input("""que desea minar?
-                                    1-diamantes
-                                    2-palos"""))
+                try:
                     while True:
-                        match minar:
+                        menu=int(input("""escoja la opcion que desea
+                                    1-paracetamol-1.500$
+                                    2-dipirona-3.000$
+                                    3-algodon-500$
+                                    4-alcohol-5.000$
+                                    5-pagar """))
+                        match menu:
                             case 1:
-                                print("minando")
-                                # time.sleep(3)
-                                probabilidad=random.randint(1,7)
-                                if probabilidad==1:
-                                    print("usted ha minado un diamante!!!")
-                                    diamante+=1
-                                else:
-                                    print("usted consiguio un carbon")
-                                    carbon+=1
-                                salir=int(input("""desea salir?
-                                                1-si
-                                                2-no"""))
-                                match salir:
-                                    case 1:
-                                        print("saliendo")
-                                        break
+                                paracetamol+=1500
+                                total_compras+=1
+                                ganancias.append(1500)
+                                total_usuario+=1500
                             case 2:
-                                print("minando")
-                                # time.sleep(3)
-                                probabilidad=random.randint(1,3)
-                                if probabilidad==1:
-                                    print("usted ha minado un palo")
-                                    palo+=1
-                                else:
-                                    print("consigui un carbon")
-                                    carbon+=1
-                                    salir=int(input("""desea salir?
-                                                1-si
-                                                2-no"""))
-                                match salir:
-                                    case 1:
-                                        print("saliendo")
-                                        break
+                                dipirona+=3000
+                                total_compras+=1
+                                ganancias.append(3000)
+                                total_usuario+=3000
+                            case 3:
+                                algodon+=500
+                                total_compras+=1
+                                ganancias.append(500)
+                                total_usuario+=500
+                            case 4:
+                                alcohol+=5000
+                                total_compras+=1
+                                ganancias.append(5000)
+                                total_usuario+=5000
+                            case 5:
+                                while True:
+                                    print(f"su total a pagar es de {total_usuario}")
+                                    rut2=int(input("ingrese su rut "))
+                                    if rut2==rut:
+                                        pase1=int(input("ingrese su contraseña "))
+                                        if pase1==pase:
+                                            pagar=int(input("ingrese el monto a pagar "))
+                                            if pagar>=total_usuario:
+                                                print(f"perfecto, su vuelto es de {pagar-total_usuario}")
+                                                print(f""" señor@ {nombre} su boleta es:
+                                                nombre:{nombre}
+                                                rut:{rut}
+                                                total de productos comprados:{total_compras}
+                                                monto a pagado {total_usuario}
+                                                vuelto: {pagar-total_usuario}""")
+                                                break
+                                            else:
+                                                print("el monto que debe pagar debe ser igual o mayor al monto a pagar")
+                                                intento1-=1
+                                                print(f"intentos:{intento1}")
+                                                if intento1==0:
+                                                    break
+                                        else:
+                                            ("contraseña incorrecta")
+                                            intento2-=1
+                                            print(f"intentos:{intento2}")
+                                            if intento2==0:
+                                                break
+                                    else:
+                                        print("rut incorrecto")
+                                        intento3-=1
+                                        print(f"intentos:{intento3}")
+                                        if intento3==0:
+                                            break
+                                break
+                            case _:
+                                print("ingrese una opcion valida")
+                except Exception:
+                    print("ingrese una opcion valida")    
             case 2:
-                while True:
-                    print("necesita dos diamantes y un palo para craftear una espada")
-                    espada=int(input("""desea craftear una espada de diamante?
-                                    1-si
-                                    2-no"""))
-                    match op:
-                        case 1:
-                            if diamante<=1 or palo<=0:
-                                print("usted no tiene recursos suficientes, consiga recursos para craftear una espada de diamante")
-                            elif diamante>=2 and palo>=1:
-                                palo-=1
-                                diamante-=2
-                                print("usted ha crafteado una espada de diamante")
-                                espada_diamante+=1
-                                print(f"espada de diamante={espada_diamante}")
-                        case 2:
-                            print("saliendo")
-                            break
-            case 3:
-                print(f"espada de diamante={espada_diamante}")
-                print(f"diamantes={diamante}")
-                print(f"palos={palo}")
-                print(f"carbon={carbon}")
-            case 4:
                 print("saliendo")
                 break
     except Exception:
         print("ingrese una opcion valida")
+
+
                         
 
 
